820274fb0635216c19cbb61922832517ca8eee1b
Theodor Hillmann <theo.hillmann@gmail.com>
2022-05-30 20:28:00 +0200

    simplified by getting rid of buttons in favour of radios & embracing css variables for easier configuration

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // CONFIGURATION ⚙️
 
 const config = {
+  sliderPosition: {
+    min: 3,
+    max: 100,
+    initial: 51,
+  },
   colors: {
     primary: "#7000ff",
     background: "#fff",
@@ -11,10 +16,11 @@ const config = {
     6: 0.06,
     12: 0.085,
   },
+  initialDiscount: 1,
   discount: {
-    marketing: 0.235294118,
-    inventory: 1,
-    other: 1,
+    1: 1,
+    2: 0.8235294118,
+    3: 1,
   },
 };
 
@@ -28,27 +34,25 @@ function round(value, precision) {
   var multiplier = Math.pow(10, precision || 0);
   return Math.round(value * multiplier) / multiplier;
 }
-let tempSliderPosition = 0.5
+let tempSliderPosition = 0.5;
 let loanAmount = config.loanAmount;
 let term = config.initialTerm;
 let oneTimeFee = config.feeRatio[term] * 100;
 let totalRepayable = loanAmount + loanAmount * oneTimeFee;
-const marketingDiscount = 0.8235294118;
-const inventoryDiscount = 1;
-const otherDiscount = 1;
 let lastSliderValue = 0.5;
+let chosenDiscount = config.initialDiscount;
 
 var isMarketingBtnActive = false;
 var isInventoryBtnActive = false;
 var isOtherBtnActive = true;
 
 let updateSlider = function (ratio) {
-  tempSliderPosition = ratio
+  tempSliderPosition = ratio;
   loanAmount = round(config.initialLoanAmount * round(ratio, 2), 0);
   oneTimeFee = round(config.feeRatio[term] * 100, 2);
   totalRepayable = round(loanAmount + loanAmount * config.feeRatio[term], 0);
-  if (isMarketingBtnActive) {
-    addDiscount(config.discount.marketing);
+  if (chosenDiscount !== 1) {
+    addDiscount(chosenDiscount);
   }
   const loanAmountElement = document.querySelector("#loanAmount");
   const totalRepayableElement = document.querySelector("#totalRepayable");
@@ -66,8 +70,7 @@ function addDiscount(discount) {
   var tempOneTimeFee = round(tempFeeRatio * 100, 1);
   oneTimeFee = tempOneTimeFee;
   totalRepayable = round(tempLoanAmount + tempFeeRatio * tempLoanAmount, 0);
-
-  //var loanAmoumtElement = document.querySelector('#loanAmount');
+  chosenDiscount = discount;
   var totalRepayableElement = document.querySelector("#totalRepayable");
   var oneTimeFeeElement = document.querySelector("#oneTimeFee");
 
@@ -81,7 +84,6 @@ let sliderChange = function (n) {
   var divider = e / 100;
   lastSliderValue = divider;
   updateSlider(divider);
-  console.log(e);
 };
 
 var triggerButtonStyle = function (button, on) {
@@ -92,14 +94,21 @@ var triggerButtonStyle = function (button, on) {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  // just to be sure
-  updateSlider(tempSliderPosition);
-
   // DOM ELEMENTS 🎨
+  const root = document.documentElement;
   const sliderInput = document.querySelector("#sliderInput");
-  const marketingBtn = document.querySelector("#marketingBtn");
-  const inventoryBtn = document.querySelector("#inventoryBtn");
-  const otherBtn = document.querySelector("#otherBtn");
+
+  // create CSS variables
+  root.style.setProperty("--widget-primary", config.colors.primary);
+  root.style.setProperty("--widget-background", config.colors.background);
+  sliderInput.style.setProperty("--min", config.sliderPosition.min);
+  sliderInput.style.setProperty("--max", config.sliderPosition.max);
+
+  // needs to be a bit more the half the width  (HACK)
+  sliderInput.style.setProperty("--value", config.sliderPosition.initial);
+
+  // enforce slider to initial value
+  updateSlider(tempSliderPosition);
 
   // THE SLIDER  🎨
   sliderInput.addEventListener("input", sliderChange);
@@ -113,52 +122,27 @@ document.addEventListener("DOMContentLoaded", function () {
     e.addEventListener("input", () => e.style.setProperty("--value", e.value));
   }
   // add an event listener for the change event
-  const radioButtons = document.querySelectorAll('input[name="termDuration"]');
-  for (const radioButton of radioButtons) {
+  const termRadio = document.querySelectorAll('input[name="termDuration"]');
+  for (const radioButton of termRadio) {
     radioButton.addEventListener("change", changeTerm);
   }
 
   function changeTerm(e) {
-    console.log(e.target.value);
     if (e.target.value) {
-      term = e.target.value
+      term = e.target.value;
       updateSlider(tempSliderPosition);
     }
   }
 
-  marketingBtn.addEventListener("click", function (event) {
-    if (!isMarketingBtnActive) {
-      triggerButtonStyle(marketingBtn, true);
-      triggerButtonStyle(inventoryBtn, false);
-      triggerButtonStyle(otherBtn, false);
-      addDiscount(marketingDiscount);
-      isInventoryBtnActive = isOtherBtnActive = false;
-      isMarketingBtnActive = true;
-    }
-    return false;
-  });
+  const discountRadio = document.querySelectorAll('input[name="discount"]');
+  for (const radioButton of discountRadio) {
+    radioButton.addEventListener("change", changeDiscount);
+  }
 
-  inventoryBtn.addEventListener("click", function (event) {
-    if (!isInventoryBtnActive) {
-      triggerButtonStyle(marketingBtn, false);
-      triggerButtonStyle(inventoryBtn, true);
-      triggerButtonStyle(otherBtn, false);
-      addDiscount(inventoryDiscount);
-      isMarketingBtnActive = isOtherBtnActive = false;
-      isInventoryBtnActive = true;
+  function changeDiscount(e) {
+    if (e.target.value) {
+      addDiscount(config.discount[e.target.value]);
+      // updateSlider(tempSliderPosition);
     }
-    return false;
-  });
-  otherBtn.addEventListener("click", function (event) {
-    if (!isOtherBtnActive) {
-      triggerButtonStyle(marketingBtn, false);
-      triggerButtonStyle(inventoryBtn, false);
-      triggerButtonStyle(otherBtn, true);
-
-      addDiscount(otherDiscount);
-      isMarketingBtnActive = isInventoryBtnActive = false;
-      isOtherBtnActive = true;
-    }
-    return false;
-  });
+  }
 });
